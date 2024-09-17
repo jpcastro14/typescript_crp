4cca839c28881298f24dff82219e53744020632a
jpcastro14 <castrovoip@gmail.com>
2024-09-17 19:42:26 -0300

    Continuing on UseReducer study

--- crp_frontlines/src/App.tsx
+++ crp_frontlines/src/App.tsx
@@ -1,12 +1,63 @@
 import "./App.css";
-import TextField from "./components/MainForm";
+import { useReducer, useState } from "react";
+
+type Task = {
+  id: number;
+  text: string;
+};
+type Action = { type: "add"; text: string } | { type: "remove"; id: number };
+
+const initializeState: Task[] = [];
+
+function transpasser(state: Task[], action: Action) {
+  switch (action.type) {
+    case "add":
+      console.log(state);
+      return [...state, { id: Date.now(), text: action.text }];
+    case "remove":
+      return state.filter((task) => task.id !== action.id);
+  }
+}
 
 function App() {
-  const showTest = (e) => {
-    console.log(e.target.value);
-  };
+  const [input, setInput] = useState("");
 
-  return <TextField onChange={showTest} />;
+  const [state, dispatch] = useReducer(transpasser, initializeState);
+
+  function addTask() {
+    dispatch({ type: "add", text: input });
+    setInput("");
+  }
+
+  function removeTask(id: number) {
+    dispatch({ type: "remove", id });
+  }
+
+  function showState(state: Task[]) {
+    console.log(state);
+  }
+
+  return (
+    <div>
+      <input
+        type="text"
+        onChange={(e) => setInput(e.target.value)}
+        value={input}
+      />
+      <button onClick={addTask}>Enviar</button>
+      <button onClick={() => showState(state)}>Mostrar State</button>
+      <ul>
+        {state.map((item) => (
+          <li key={item.id}>
+            <span>
+              {item.text}
+              <button onClick={() => removeTask(item.id)}>Remove</button>
+            </span>
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
 }
 
 export default App;
--- crp_frontlines/src/components/MainForm/index.tsx
+++ crp_frontlines/src/components/MainForm/index.tsx
@@ -1,15 +1,12 @@
 import { useRef, FunctionComponent } from "react";
 
-interface TextFieldProps {
-  onChange: React.ChangeEventHandler<HTMLInputElement> | undefined;
-}
-
 const TextField: FunctionComponent<TextFieldProps> = ({ onChange }) => {
   const inputRef = useRef<HTMLInputElement>(null);
 
   return (
     <div>
       <input ref={inputRef} onChange={onChange} />
+      <ul></ul>
     </div>
   );
 };
