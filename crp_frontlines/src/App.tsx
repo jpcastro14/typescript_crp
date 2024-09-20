4ade3879e0fd8f70ac87c15e8359c4d704e9d297
jpcastro14 <castrovoip@gmail.com>
2024-09-20 18:53:30 -0300

    Continuing on learning useReducer Hook

--- crp_frontlines/src/App.tsx
+++ crp_frontlines/src/App.tsx
@@ -1,37 +1,61 @@
 import "./App.css";
 
-import { useReducer } from "react";
+import { useReducer, useState } from "react";
 
-type State = {
-  count: number;
+type User = {
+  age: number;
+  text: string;
 };
 
-type Action = | { type: "add" } | { type: "decrement" };
+type Action = { type: "add"; age: number; text: string };
 
-const initializestate = { count: 0 };
+const initialState: User[] = [];
 
-function reducer(state: State, action: Action) {
+function reducer(state: User[], action: Action) {
   switch (action.type) {
     case "add":
       console.log(state);
-      console.log(action.type);
-      return { count: state.count + 1 };
-    case "decrement":
-      console.log(state);
-      console.log(action.type);
-      return { count: state.count - 1 };
+      return [...state, { age: action.age, text: action.text }];
   }
 }
 
 function App() {
-  const [state, dispatch] = useReducer(reducer, initializestate);
+  const [input, setInput] = useState("");
+  const [age, setAge] = useState(0);
+
+  const [state, dispatch] = useReducer(reducer, initialState);
+
+  function addTask() {
+    dispatch({ type: "add", text: input, age: age });
+    setInput("");
+    setAge(0);
+  }
 
   return (
     <>
       <div>
-        <h1>{state.count}</h1>
-        <button>-</button>
-        <button onClick={() => dispatch({ type: "add" })}>+</button>
+        <input
+          type="text"
+          value={input}
+          onChange={(event) => setInput(event.target.value)}
+        />
+        <br />
+        <input
+          type="text"
+          value={age}
+          onChange={(e) => setAge(e.target.value)}
+        />
+        <br />
+        <button onClick={addTask}>Add</button>
+      </div>
+      <div>
+        {state.map((item) => (
+          <p>
+            {item.text}
+            {"  "}
+            {item.age}
+          </p>
+        ))}
       </div>
     </>
   );
