ce8edb6b0cd0f50bfec0850a4bc9112550bd23f0
jpcastro14 <castrovoip@gmail.com>
2024-10-06 18:07:58 -0300

    Adding JSON-SERVER to the project, in order to continue on developing front end features without the need to wait for the backend to be ready

--- crp_frontlines/src/components/EventDash/index.tsx
+++ crp_frontlines/src/components/EventDash/index.tsx
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { FunctionComponent, useEffect, useState} from "react";
 import {
   Container,
   HeaderInfo,
@@ -13,15 +13,37 @@ import {
   DescriptionField,
   BodyInfo,
 } from "./styles";
-
 import expand from "../assets/expand.svg";
 
-function EventDash() {
-  const [open, setOpen] = useState(false);
+type Event = {
+  id:number;
+  title:string;
+  eventSector:string;
+  eventArea:string;
+  eventCriticality: string;
+  eventPriority:string;
+  eventDescription: string;
+}
 
-  function toggleState() {
-    setOpen(!open);
-  }
+const EventDash:FunctionComponent =() => {
+  const [posts, setPosts] = useState<Event | undefined>()
+  const [open, setOpen] = useState<boolean>(false);
+
+  useEffect(()=> {
+    
+    const getPosts = async () =>{
+      const response = await fetch(`http://localhost:4000/posts/1/`);
+      const data = await response.json()
+      console.log(data);
+      
+      setPosts(data)
+    }
+
+    getPosts();
+  },[])
+
+
+
 
   return (
     <>
@@ -30,12 +52,12 @@ function EventDash() {
           <span className="EventCategory"></span>
           <div className="EventType"></div>
           <EventTitle>
-            <span>Queda Servidor LDAP</span>
+            <span>{posts?.title}</span>
             <p>Ocorrido: Segunda Feira, 30 de setembro as 19:51</p>
           </EventTitle>
           <EventAction>
             <button></button>
-            <button onClick={toggleState}>
+            <button onClick={()=> setOpen(!open)}>
               <img src={expand} />
             </button>
             <button>c</button>
@@ -47,37 +69,26 @@ function EventDash() {
             {/* ------------------Setor----------------------Ele [e teu p--*/}
             <PointerContainer>
               <label>Setor</label>
-              <SectorButton>SEARP - TELARUS</SectorButton>
+              <SectorButton>{posts?.eventSector}</SectorButton>
             </PointerContainer>
             {/* ------------------Area--------------------------*/}
             <PointerContainer>
               <label>Area Afetada</label>
-              <AreaButton>Infraestrutura</AreaButton>
+              <AreaButton>{posts?.eventArea}</AreaButton>
             </PointerContainer>
             {/* ------------------Criticality------------------ */}
             <PointerContainer>
               <label>Criticalidade</label>
-              <CriticButton levelColor={"#157145"}>Baixa</CriticButton>
+              <CriticButton levelColor={"#157145"}>{posts?.eventCriticality}</CriticButton>
             </PointerContainer>
             {/* ------------------Priority--------------------- */}
             <PointerContainer>
               <label>Prioridade</label>
-              <PriorityButton levelColor={"#fed766"}>Média</PriorityButton>
+              <PriorityButton levelColor={"#fed766"}>{posts?.eventPriority}</PriorityButton>
             </PointerContainer>
           </PointerField>
           <DescriptionField>
-            There are many variations of passages of Lorem Ipsum available, but
-            the majority have suffered alteration in some form, by injected
-            humour, or randomised words which don't look even slightly
-            believable. If you are going to use a passage of Lorem Ipsum, you
-            need to be sure there isn't anything embarrassing hidden in the
-            middle of text. All the Lorem Ipsum generators on the Internet tend
-            to repeat predefined chunks as necessary, making this the first true
-            generator on the Internet. It uses a dictionary of over 200 Latin
-            words, combined with a handful of model sentence structures, to
-            generate Lorem Ipsum which looks reasonable. The generated Lorem
-            Ipsum is therefore always free from repetition, injected humour, or
-            non-characteristic words etc.
+            {posts?.eventDescription}
           </DescriptionField>
         </BodyInfo>
       </Container>
