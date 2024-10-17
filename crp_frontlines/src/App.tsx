50e077af3ebcbc235668daec818086dc7c7045c9
jpcastro14 <castrovoip@gmail.com>
2024-10-17 19:20:48 -0300

    Creating form container

--- crp_frontlines/src/App.tsx
+++ crp_frontlines/src/App.tsx
@@ -32,7 +32,7 @@ function App() {
     // eslint-disable-next-line react-hooks/exhaustive-deps
   }, []);
 
-  /*  const event = [
+  const event = [
     {
       id: 1,
       eventTitle: "Queda servidor LDAP",
@@ -75,11 +75,11 @@ function App() {
       eventDescription:
         "There are many variations of passages of Lorem Ipsum available, but the majority have suffered alteration in some form, by injected humour, or randomised words which don't look even slightly believable. If you are going to use a passage of Lorem Ipsum, you need to be sure there isn't anything embarrassing hidden in the middle of text. All the Lorem Ipsum generators on the Internet tend to repeat predefined chunks as necessary, making this the first true generator on the Internet. It uses a dictionary of over 200 Latin words, combined with a handful of model sentence structures, to generate Lorem Ipsum which looks reasonable. The generated Lorem Ipsum is therefore always free from repetition, injected humour, or non-characteristic words etc.",
     },
-  ]; */
+  ];
 
   return (
     <>
-      {evData.map((item) => (
+      {event.map((item) => (
         <EventDash key={item.id} data={item} />
       ))}
     </>
