50f4c7255a15539252b6e586c01655fe1e08154c
jpcastro14 <castrovoip@gmail.com>
2024-11-19 19:27:24 -0300

    Continuing developing IssueForm component

--- crp_frontlines/src/components/Forms/styles.ts
+++ crp_frontlines/src/components/Forms/styles.ts
@@ -3,6 +3,6 @@ import styled from "styled-components";
 export const Container = styled.div`
     display: flex;
     height: 200px;
-    width: 200px;
+    width: 100%;
     background-color: red;
 `;
--- crp_frontlines/src/components/Input/styles.ts
+++ crp_frontlines/src/components/Input/styles.ts
@@ -1,5 +1,7 @@
 import styled from "styled-components";
 
+
 export const InputContainer = styled.div`
     background-color: blue;
+    width: 100%;
 `;
