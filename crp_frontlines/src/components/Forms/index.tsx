86aecb823d291e1a02cd741b8c7c3d4b904fd7cd
jpcastro14 <castrovoip@gmail.com>
2024-11-19 19:46:49 -0300

    Testing other branch and push options

--- crp_frontlines/src/components/Forms/index.tsx
+++ crp_frontlines/src/components/Forms/index.tsx
@@ -1,15 +1,15 @@
 import Input from "../Input";
 import { Container } from "./styles";
 
-
-
-
 function IssueForm() {
-    return <Container>
-        <Input type='text' placeholder="Escreva aqui" />
-        <Input type='number' placeholder="Teste placeholder" />
-        <Input type='number' placeholder="Teste placeteste" />
+  return (
+    <Container>
+      <Input type="text" placeholder="Escreva aqui" />
+      <Input type="number" placeholder="Teste placeholder" />
+      <Input type="number" placeholder="Teste placeteste" />
+      <Input type="text" placeholder="Teste de git e branch" />
     </Container>
+  );
 }
 
 export default IssueForm;
