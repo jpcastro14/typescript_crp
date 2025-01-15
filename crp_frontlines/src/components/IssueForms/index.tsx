fa0bf59fd2687999ace70d3827ac9b43ee177f7d
jpcastro14 <castrovoip@gmail.com>
2025-01-15 18:52:05 -0300

    Adding React Hook Form library to manage and validate forms

--- crp_frontlines/src/components/IssueForms/index.tsx
+++ crp_frontlines/src/components/IssueForms/index.tsx
@@ -17,6 +17,7 @@ import {
 import { ReactNode, useState } from "react";
 import headset from "../assets/headset.svg";
 import AlertMessage from "../Messages/AlertMessage";
+import { useForm } from "react-hook-form";
 
 type mainIssue = {
   id?: number;
@@ -38,20 +39,14 @@ type messageProps = {
 }
 
 function IssueForm() {
+  const { register, handleSubmit } = useForm();
   const [open] = useState<boolean>(true);
   const [fixedState, setFixedState] = useState<mainIssue | undefined>(undefined);
   const [messageConfig, setMessageConfig] = useState<messageProps>({ trigger: false, alertText: "", variant: "" })
 
 
-  function handleType(e: React.ChangeEvent<HTMLInputElement | HTMLTextAreaElement | HTMLSelectElement>) {
-    const { name, value } = e.target;
-    setFixedState((prevState) => ({
-      ...prevState,
-      active: true,
-      [name]: value,
-    }));
-
-
+  const onSubmit = (data: mainIssue) => {
+    console.log(data);
   }
 
   function handlePost() {
@@ -76,95 +71,19 @@ function IssueForm() {
       )
   }
 
-  function handleSelect(e: React.ChangeEvent<HTMLSelectElement>) {
-    const { name, selectedIndex } = e.target;
-
-    switch (selectedIndex) {
-      case 1:
-        setFixedState((prevState) => ({
-          ...prevState,
-          [name]: selectedIndex,
-          eventCriticality: selectedIndex,
-          eventCriticalityColor: "var(--primary-blue)",
-        }))
-        setMessageConfig({ trigger: true, alertText: `Chamado com criticalidade ${selectedIndex}`, variant: "primary" })
-        break
-      case 2:
-        setFixedState((prevState) => ({
-          ...prevState,
-          [name]: selectedIndex,
-          eventCriticality: selectedIndex,
-          eventCriticalityColor: 'var(--primary-yellow)',
-        }))
-        setMessageConfig({ trigger: true, alertText: `Chamado com criticalidade ${selectedIndex}`, variant: "warning" })
-        break
-      case 3:
-        setFixedState((prevState) => ({
-          ...prevState,
-          [name]: selectedIndex,
-          eventCriticality: selectedIndex,
-          eventCriticalityColor: 'var(--primary-red)'
-        }))
-        setMessageConfig({ trigger: true, alertText: `Chamado com criticalidade ${selectedIndex}`, variant: "danger" })
-        break
-      default:
-        break;
-    }
-  }
-
-  function handlePriority(e: React.ChangeEvent<HTMLSelectElement>) {
-    const { selectedIndex } = e.target;
-
-    switch (selectedIndex) {
-      case 1:
-        setFixedState((prevState) => ({
-          ...prevState,
-          eventPriority: selectedIndex
-        }))
-        setMessageConfig({ trigger: true, alertText: `Chamado com prioridade ${selectedIndex}`, variant: 'primary' })
-        break;
-      case 2:
-        setFixedState((prevState) => ({
-          ...prevState,
-          eventPriority: selectedIndex
-        }))
-        setMessageConfig({ trigger: true, alertText: `Chamado com prioridade ${selectedIndex}`, variant: 'warning' })
-        break;
-      case 3:
-        setFixedState((prevState) => ({
-          ...prevState,
-          eventPriority: selectedIndex
-        }))
-        setMessageConfig({ trigger: true, alertText: `Chamado com prioridade >${selectedIndex}<, deve ser encaminhado imediatamente para o setor responsável!`, variant: 'danger' })
-    }
-
-  }
-
-  function sampleDate() {
-    const date = new Date();
-    console.log(Intl.DateTimeFormat('pt-br', {
-      dateStyle: 'full',
-      timeStyle: 'long',
-    }).format(date))
-  }
-
   return (
     <>
       <AlertMessage variant={messageConfig.variant} alertText={messageConfig.alertText} trigger={messageConfig.trigger} onClose={() => setMessageConfig({ trigger: false })} />
       <Container>
         <HeaderInfo>
           <EventCategory
-            $levelcolor={
-              fixedState?.eventCriticalityColor ? fixedState?.eventCriticalityColor : "var(--secondary-gray)"
-            }
+            $levelcolor={"var(--secondary-gray)"}
           />
           <EventType>
-            <p></p>
             <img src={headset} />
           </EventType>
           <EventTitle>
-            <span>{fixedState?.eventTitle}</span>
-            <p onClick={sampleDate}>Ocorrido:<strong>{fixedState?.eventMoment}</strong></p>
+            <span>Abertura de chamado</span>
           </EventTitle>
         </HeaderInfo>
         <BodyInfo open={open}>
@@ -172,9 +91,7 @@ function IssueForm() {
             <PointerContainer>
               <label>Título da ocorrência</label>
               <TitleInput
-                onChange={handleType}
-                name="eventTitle"
-                defaultValue={fixedState?.eventTitle}
+                {...register("eventTitle")}
               />
             </PointerContainer>
           </PointerField>
@@ -185,58 +102,46 @@ function IssueForm() {
             <PointerContainer>
               <label>Setor</label>
               <SectorInput
-                onChange={handleType}
-                name="eventSector"
-                defaultValue={fixedState?.eventSector}
+                {...register("eventSector")}
               />
             </PointerContainer>
             {/* ------------------Area--------------------------*/}
             <PointerContainer>
               <label>Area Afetada</label>
               <AreaInput
-                onChange={handleType}
-                name="eventArea"
-                defaultValue={fixedState?.eventArea}
+                {...register("eventArea")}
               />
             </PointerContainer>
             {/* ------------------Criticality------------------ */}
             <PointerContainer>
               <label>Criticalidade</label>
               <CriticSelect
-                name='eventCriticality'
-                defaultValue={'...'}
-                value={fixedState?.eventCriticality}
-                onChange={handleSelect}
+                {...register("eventCriticality")}
               >
                 <option>...</option>
-                <option>1</option>
-                <option>2</option>
-                <option>3</option>
+                <option value='1'>1</option>
+                <option value='2'>2</option>
+                <option value='3'>3</option>
               </CriticSelect>
             </PointerContainer>
-            {/* ------------------Priority--------------------- */}
+            {/* ------------------Prioridade--------------------- */}
             <PointerContainer>
               <label>Prioridade</label>
               <PrioritySelect
-                name="eventPriority"
-                defaultValue={'...'}
-                value={fixedState?.eventPriority}
-                onChange={handlePriority}
+                {...register("eventPriority")}
               >
                 <option>...</option>
-                <option>1</option>
-                <option>2</option>
-                <option>3</option>
+                <option value='1'>1</option>
+                <option value='2'>2</option>
+                <option value='3'>3</option>
               </PrioritySelect>
             </PointerContainer>
           </PointerField>
           <DescriptionField
-            name="eventDescription"
-            onChange={handleType}
-            defaultValue={fixedState?.eventDescription}
-            placeholder="Descrição do evento" />
+            {...register("eventDescription")}
+          />
         </BodyInfo>
-        <button onClick={handlePost} >Teste</button>
+        <button onClick={() => handleSubmit(onSubmit)()} >Teste</button>
       </Container>
     </>
   );
